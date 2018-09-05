2ec5d834aaafa8b052862f4f270d889f0f764780
SisterWang <cjp0122@163.com>
2018-09-05 17:04:30 +0800

    添加header

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import fastClick from 'fastclick'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
-import fastClick from 'fastclick'
+import './assets/styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
@@ -12,10 +13,3 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
-
-// new Vue({
-// 	el: 'app',
-// 	router,
-// 	components: { App: App },
-// 	template: '<App/>'
-// })
