e0b3817df283580b36e645e655610d50ac735548
SisterWang <cjp0122@163.com>
2018-09-05 18:33:58 +0800

    增加轮播功能

---
+++ src/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://img1.qunarzz.com/vc/99/c1/c4/3426c6a3b54ef464e40e440fe3.jpg'
+      },{
+      	id: '0002',
+      	imgUrl: 'https://img1.qunarzz.com/vc/ce/4b/45/7f78e2003fb5fb65c57eb1a800.jpg'
+      },{
+      	id: '0003',
+      	imgUrl: 'https://img1.qunarzz.com/vc/ce/4b/45/7f78e2003fb5fb65c57eb1a800.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 30.47%
+    .swiper-img
+      width: 100%
+</style>
