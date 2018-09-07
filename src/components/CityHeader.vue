3be90ae6c59037c159f1513f1784ecf035ece3bc
SisterWang <cjp0122@163.com>
2018-09-07 16:06:34 +0800

    完成首页和城市页面的路由

---
+++ src/components/CityHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="header">
+      <router-link to='/'>
+        <div class="iconfont back-btn">&#xe624;</div>
+      </router-link>城市选择
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '../assets/styles/varibles.styl'
+  .header
+    overflow: hidden
+    height: .86rem
+    line-height: .86rem
+    text-align: center
+    color: #fff
+    background: $bgColor
+    font-size: .32rem
+    .back-btn
+      position: absolute
+      top: 0
+      left: 0
+      width: .64rem
+      text-align: center
+      font-size: .4rem
+      color: #fff
+</style>
