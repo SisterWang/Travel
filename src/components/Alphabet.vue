9e9c45ff5febb8e8f5647477d8dae068b029d778
SisterWang <cjp0122@163.com>
2018-09-07 21:10:31 +0800

    完成城市页面布局
    使用了better-scroll插件

---
+++ src/components/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <ul class="list">
+      <li class="item">a</li>
+      <li class="item">b</li>
+      <li class="item">c</li>
+      <li class="item">d</li>
+      <li class="item">e</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      @import '../assets/styles/varibles.styl'
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
--- src/views/City.vue
+++ src/views/City.vue
@@ -2,18 +2,24 @@
     <div>
       <city-header></city-header>
       <city-search></city-search>
+      <city-list></city-list>
+      <alphabet></alphabet>
     </div>
 </template>
 
 <script>
 import CityHeader from '@/components/CityHeader.vue'
 import CitySearch from '@/components/CitySearch.vue'
+import CityList from '@/components/CityList.vue'
+import Alphabet from '@/components/Alphabet.vue'
 
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    Alphabet
   }
 }
 </script>
